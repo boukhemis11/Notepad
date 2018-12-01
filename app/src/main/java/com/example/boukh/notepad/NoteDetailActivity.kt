70bab882f95aea42e1494ce78a8c3d3f3d720504
boukhemis11 <boukhemis11@gmail.com>
2018-12-01 15:19:14 +0100

    add note

--- app/src/main/java/com/example/boukh/notepad/NoteDetailActivity.kt
+++ app/src/main/java/com/example/boukh/notepad/NoteDetailActivity.kt
@@ -1,5 +1,7 @@
 package com.example.boukh.notepad
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
@@ -10,6 +12,7 @@ import android.widget.TextView
 class NoteDetailActivity : AppCompatActivity() {
 
     companion object {
+        val REQUEST_EDIT_NOTE = 1
         val EXTRA_NOTE ="note"
         val EXTRA_NOTE_INDEX ="noteIndex"
     }
@@ -46,9 +49,21 @@ class NoteDetailActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.action_save -> {
+                saveNote()
             return true
         }
         else -> return super.onOptionsItemSelected(item)
         }
+    }
+
+    fun saveNote() {
+        note.title = titleView.text.toString()
+        note.text = textView.text.toString()
+
+        intent = Intent()
+        intent.putExtra(EXTRA_NOTE, note)
+        intent.putExtra(EXTRA_NOTE_INDEX,noteIndex)
+        setResult(Activity.RESULT_OK, intent)
+        finish()
     }
 }
--- app/src/main/java/com/example/boukh/notepad/NoteListActivity.kt
+++ app/src/main/java/com/example/boukh/notepad/NoteListActivity.kt
@@ -1,8 +1,10 @@
 package com.example.boukh.notepad
 
+import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
@@ -18,14 +20,16 @@ class NoteListActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_list)
 
-        var toolbar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolbar)
-
-        notes = mutableListOf<Note>()
+        notes = mutableListOf()
         notes.add(Note("Note1", "Bla1"))
         notes.add(Note("Note2", "Bla2"))
         notes.add(Note("Note3", "Bla3"))
         notes.add(Note("Note4", "Bla4"))
+
+        var toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        findViewById<FloatingActionButton>(R.id.creat_note_fab).setOnClickListener(this)
 
         adapter = NoteAdapter(notes,this)
         val recyclerView = findViewById<RecyclerView>(R.id.notes_recycle_view)
@@ -33,17 +37,51 @@ class NoteListActivity : AppCompatActivity(), View.OnClickListener {
         recyclerView.adapter = adapter
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode != Activity.RESULT_OK || data == null) {
+            return
+        }
+        when (requestCode){
+            NoteDetailActivity.REQUEST_EDIT_NOTE -> processeEditNoteResult(data)
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun processeEditNoteResult(data: Intent) {
+        val noteIndex = data.getIntExtra(NoteDetailActivity.EXTRA_NOTE_INDEX, -1)
+        val note = data.getParcelableExtra<Note>(NoteDetailActivity.EXTRA_NOTE)
+        saveNote(note, noteIndex)
+    }
+
     override fun onClick(view: View) {
-            if (view.tag != null){
-                showNoteDetail(view.tag as Int)
+        if (view.tag != null){
+            showNoteDetail(view.tag as Int)
+        } else {
+            when (view.id) {
+                R.id.creat_note_fab -> creatNewNote()
             }
+        }
+    }
+
+    fun saveNote(note: Note, noteIndex: Int) {
+        if (noteIndex < 0){
+            notes.add(0, note)
+        }else {
+            notes[noteIndex] = note
+        }
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun creatNewNote() {
+        showNoteDetail(-1)
     }
 
     fun showNoteDetail(noteIndex: Int){
-        val note = notes[noteIndex]
+        val note = if (noteIndex < 0) Note() else notes[noteIndex]
+
         val intent = Intent(this, NoteDetailActivity::class.java)
         intent.putExtra(NoteDetailActivity.EXTRA_NOTE, note)
         intent.putExtra(NoteDetailActivity.EXTRA_NOTE_INDEX, noteIndex)
-        startActivity(intent)
+        startActivityForResult(intent, NoteDetailActivity.REQUEST_EDIT_NOTE)
     }
 }
